18e2dcf97504a8b556acd63617e0608e4781f10d
kryptocodes <srivatsantb@gmail.com>
2019-10-06 19:47:17 +0530

    added venue

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,13 +27,10 @@ const IndexPage = () => (
          <img src={bg}/>
          <div ClassName="group">
         <h1 className="mb-1">Amritagiri</h1>
-        <h2>October 18</h2>
-        <Scroll type="id" element="register" offset={100}>
-          
-          <a className="btn  btn-xl" href="#about">
+        <h2>October 18</h2>  
+          <a className="btn  btn-xl" href="https://forms.gle/kSiMSXj1TVivP2y78">
             Register
           </a>
-        </Scroll>
       <Scroll type="id" element="about" offset={50}>
       <div class="scroll-downs">
         <div class="mousey">
@@ -71,43 +68,30 @@ const IndexPage = () => (
     </section>
 
     
-    <section id="register" className="callout">
-      <div className="container text-center">
-        <h2 className="mx-auto mb-5">
-          Let's Hack for
-          <em><br/>Open Source</em>
-          <em><br/>together</em>
-        </h2>
-        <a
-          className="button"
-          href="https://forms.gle/kSiMSXj1TVivP2y78"
-        >
-          <span>Register here</span>
-        </a>
+    
+    <section id="venue" className="venue-details">
+      <div className="row m-0 w-100">
+        <div className="col-lg-8 order-2 p-4">
+          <iframe
+            className="location-map"
+            src="https://maps.google.com/maps?q=Amrita%20college%20of%20engineering%20and%20technology&t=&z=17&ie=UTF8&iwloc=&output=embed"
+            frameBorder="0"
+            allowFullScreen={false}
+          >
+          </iframe>
+        </div>
+        <div className="col p-4">
+          <h2>Venue</h2>
+          <h3>Amrita College of Engineering and Technology,</h3>
+          <div className="fs-5">
+          Erachakulam,Nagercoil
+          </div>
+          <div className="fs-6">
+          Tamilnadu, India - 629 901
+          </div>
+        </div>
       </div>
     </section>
-
-    
-    <section id="contact" className="map">
-    <div className="mapp">
-    <h1>Location</h1>
-    <h2>TBA</h2>
-    </div>
-    <iframe
-      width="100%"
-      height="100%"
-      frameborder="0"
-      scrolling="no"
-      marginheight="0"
-      marginwidth="0"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d893.3517971060317!2d77.41480472917415!3d8.228518926928366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f71ade80cd81%3A0xaa938f88af79cc54!2sAmirtha+Institutions!5e1!3m2!1sen!2sin!4v1558498717706!5m2!1sen!2sin"
-    ></iframe>
-    <br />
-    <small>
-      <a href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d893.3517971060317!2d77.41480472917415!3d8.228518926928366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b04f71ade80cd81%3A0xaa938f88af79cc54!2sAmirtha+Institutions!5e1!3m2!1sen!2sin!4v1558498717706!5m2!1sen!2sin"></a>
-    </small>
-  </section>
-
     <ScrollToTop />
     <Footer />
   </Layout>
