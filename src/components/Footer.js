cc714d074cf3aa08eb4f144b04ba13b058626fc8
kryptocodes <srivatsantb@gmail.com>
2019-10-08 14:04:55 +0530

    footer txt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="footer text-center">
       <div className="container">
-        <h1>Contact</h1>
+        <h1>An initiative by open minds for open minds</h1>
         <ul className="list-inline mb-5">
         </ul>
         <p className="text-muted small mb-0">
